d9c6e29c90505f8a08646e94f88cfa96e1dabee4
Paul01001000 <go82lok@mytum.de>
2026-05-13 18:49:19 +0200

    Add baseUrl to orval config

--- web-client/orval.config.ts
+++ web-client/orval.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'orval';
 
+const baseUrl = "localhost:8080";
+
 export default defineConfig({
   genai: {
     input: '../api/genAI.yaml',
     output: {
       client: 'fetch',
+      baseUrl,
       target: 'src/api/genai.ts',
     },
   },
