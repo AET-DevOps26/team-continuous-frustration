dd4bc0235cc1995d59c903364785a2a468c681cd
Paul01001000 <go82lok@mytum.de>
2026-05-13 18:32:51 +0200

    Add script and orval config

--- api/scripts/gen-all.sh
+++ api/scripts/gen-all.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-openapi-generator-cli generate -i api/openapi.yaml -g spring \
-  -o services/spring-order/generated --skip-validate-spec
+npx @redocly/cli lint api/*.yaml
 
-openapi-generator-cli generate -i api/openapi.yaml -g python-fastapi \
-  -o services/py-order/generated --skip-validate-spec
+openapi-generator-cli generate -i api/genai.yaml -g python-fastapi \
+  -o services/py-GenAI/generated --skip-validate-spec
 
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-npm --prefix "$ROOT_DIR/web-client" run orval
+npm --prefix "./web-client" run orval
