57555f1b765f33095994189ab76e0ddb6c8568a3
Paul01001000 <go82lok@mytum.de>
2026-07-12 17:33:32 +0200

    fix: remove unused import

--- web-client/src/pages/StudySessionPage.tsx
+++ web-client/src/pages/StudySessionPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { ChevronLeft, ChevronDown, ChevronUp, Sparkles, X, Square, Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
